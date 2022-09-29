0aa9d031f3204e060489b4d40e2df4011f5075e5
ranchokim64 <ranchokim64@gmail.com>
2022-09-29 22:18:20 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb+srv://jonghyeonkim:920210@boilerplate.4ve0r0l.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err))
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
